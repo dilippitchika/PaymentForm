ae5f0b3b4d0d428ad4459371bd69077beb11374d
dilip pitchika <dilippitchika@gmail.com>
2020-02-28 01:15:38 +0530

    Updated code

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -17,16 +17,30 @@ export class DataService {
 
   postPaymentSettingsForm(uid: string) {
 
-    const headers = { 'bot-access-token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NzMwLCJyb2xlIjoiYm90In0.ea4Ge6I5uGjW6j7ZICTsJ1BBJFKS-mWYTAwrXacm0-I', 'Content-type': 'application/json' };
+    const headers = {
+      'Content-Type': 'application/json',
+      'imi_bot_middleware_token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjoiVGhpcyBpcyBJTUkgQk9UIG1pZGRsZXdhcmUiLCJpYXQiOjE1Njc4ODc5MTAsImV4cCI6NDE1OTg4NzkxMH0.dYbMaf8HYMD5K532p7DpHN0cmru-JKMjst-WS9zi7u8'
+    };
 
 
     const body = {
-      'consumer': { 'uid': uid }, 'msg': '123bps123',
-      'platform': 'web',
-      'type': 'human'
-    };
+      "consumer":{
+          "namespace": "BOT",
+          "socket_key": uid
+      },
+      "event":"preview",
+      "payload":{
+      "generated_msg":[
+          {
+              "text": "Your payment of JOD 7.87 was successful! Thanks you for choosing Acme Telecom."
+          }
+      ],
+      "language":"en",
+      "room":{}
+      }
+  }
 
-    return this.http.post('https://imibot.ai/api/v1/webhook/web/', body, { headers })
+    return this.http.post('https://rtm.imibot.ai/api/v1/socket/sendMessage', body, { headers })
       // .catch(this.handleError);
       .pipe(catchError((err) => {
         console.log('Error: Unable to complete request...', err.message);
